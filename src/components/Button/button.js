f41956f31394c246914bf25c5b02858fe0710577
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-01-19 11:26:18 +0900

    style: prettier 변경사항 반영

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,19 +1,19 @@
-import "./button.css"
+import "./button.css";
 
 // 버튼 컴포넌트 생성 함수
 function createButton(text, onClick, classNames = []) {
-  const button = document.createElement('button');
-  
+  const button = document.createElement("button");
+
   // 버튼에 들어갈 텍스트 입력
   button.textContent = text;
   // 기본 클래스 btn
-  button.classList.add('btn');
+  button.classList.add("btn");
   // 추가 클래스 있는 경우 입력
-  classNames.forEach(className => button.classList.add(className));
+  classNames.forEach((className) => button.classList.add(className));
   // 클릭 이벤트 추가
-  button.addEventListener('click', onClick);
+  button.addEventListener("click", onClick);
 
   return button;
 }
 
-export {createButton};
+export { createButton };
