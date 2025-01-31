157c26ed6bededb6a4991bdf598c7d8e82b16925
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-01-31 15:42:41 +0900

    refactor: 공지사항 날짜 포맷 로직 분리

--- src/pages/admin/notice/notice.js
+++ src/pages/admin/notice/notice.js
@@ -1,24 +1,25 @@
-import "./notice.css";
+import './notice.css';
 
-import { createButton } from "@/components/Button/button.js";
-import { createInputField } from "@/components/InputField/input.js";
+import { createButton } from '@/components/Button/button.js';
+import { createInputField } from '@/components/InputField/input.js';
+import { formatSimpleDate } from '@/utils/timeUtils';
 
 const CHECKED_ICON_URL =
-  "https://github.com/user-attachments/assets/68846971-a34b-4d1f-bc01-2422f4f5e8da";
+  'https://github.com/user-attachments/assets/68846971-a34b-4d1f-bc01-2422f4f5e8da';
 const UNCHECKED_ICON_URL =
-  "https://github.com/user-attachments/assets/34bd4a1e-73f1-4ef8-896c-1aad0c1fed99";
+  'https://github.com/user-attachments/assets/34bd4a1e-73f1-4ef8-896c-1aad0c1fed99';
 const VIEW_ICON_URL =
-  "https://github.com/user-attachments/assets/3f2ccbf5-9c2e-4284-858b-c4ef0aad4f94";
+  'https://github.com/user-attachments/assets/3f2ccbf5-9c2e-4284-858b-c4ef0aad4f94';
 const LIST_ICON_URL =
-  "https://github.com/user-attachments/assets/1dae0ec4-db39-49a3-b720-925fa237e33f";
+  'https://github.com/user-attachments/assets/1dae0ec4-db39-49a3-b720-925fa237e33f';
 
 const notice = (contents) => {
   const submitButton = createButton(
-    "등록",
+    '등록',
     () => {
-      window.location.href = "/notice/write"; // 등록 버튼 클릭 시 등록 페이지로 이동
+      window.location.href = '/notice/write'; // 등록 버튼 클릭 시 등록 페이지로 이동
     },
-    ["btn--submit"]
+    ['btn--submit'],
   );
 
   // const deleteButton = createButton(
@@ -28,16 +29,16 @@ const notice = (contents) => {
   // );
 
   const searchInput = createInputField({
-    type: "search",
+    type: 'search',
     attributes: {
-      name: "search",
-      id: "search",
+      name: 'search',
+      id: 'search',
     },
     datasets: { required: true, validation: true },
   });
 
   // 현재 페이지가 관리자용의 '공지사항관리' 페이지인지 확인
-  const isAdminNoticePage = window.location.pathname === "/admin/notice";
+  const isAdminNoticePage = window.location.pathname === '/admin/notice';
 
   contents.innerHTML = `
   <section class="wrapper">
@@ -68,25 +69,25 @@ const notice = (contents) => {
 
   // 버튼 DOM에 직접 추가
   if (isAdminNoticePage) {
-    const buttonContainer = contents.querySelector(".action-buttons");
+    const buttonContainer = contents.querySelector('.action-buttons');
     buttonContainer.appendChild(submitButton);
   }
 
   // JSON 데이터 로드
-  let noticesData = JSON.parse(localStorage.getItem("notices")) || [];
+  let noticesData = JSON.parse(localStorage.getItem('notices')) || [];
   displayNotices(noticesData);
   addClickEventToItems();
 
   // 검색 입력 이벤트 처리
-  const searchEl = document.getElementById("search");
+  const searchEl = document.getElementById('search');
 
-  searchEl.addEventListener("input", function () {
+  searchEl.addEventListener('input', function () {
     const searchTerm = searchEl.value.toLowerCase(); // 소문자로 변환하여 검색
 
     const filteredNotices = noticesData.filter(
       (notice) =>
         notice.title.toLowerCase().includes(searchTerm) ||
-        notice.contents.toLowerCase().includes(searchTerm)
+        notice.contents.toLowerCase().includes(searchTerm),
     );
 
     // 검색 결과 출력
@@ -95,28 +96,23 @@ const notice = (contents) => {
 
   // 공지사항을 출력하는 함수
   function displayNotices(data) {
-    const galleryList = document.querySelector(".gallery-list");
-    const list = document.querySelector(".list");
+    const galleryList = document.querySelector('.gallery-list');
+    const list = document.querySelector('.list');
 
     const noticesHTML = data
       .map((item) => {
         // 날짜 포맷팅 ex) 2025.01.21
         const date = new Date(item.date);
-        const formattedDate =
-          date.getFullYear() +
-          "." +
-          String(date.getMonth() + 1).padStart(2, "0") +
-          "." +
-          String(date.getDate()).padStart(2, "0");
+        const formattedDate = formatSimpleDate(date) || '날짜 정보가 없습니다.';
 
-        let imageSrc = "../../public/imgs/images.jpg";
+        let imageSrc = '../../public/imgs/images.jpg';
         if (
           item.imgSrc &&
           item.imgSrc.length > 0 &&
           Array.isArray(item.imgSrc)
         ) {
           const validImage = item.imgSrc.find((img) =>
-            /\.(jpg|jpeg|png)$/i.test(img.src)
+            /\.(jpg|jpeg|png)$/i.test(img.src),
           ); // JPG 또는 PNG 파일 찾기
           if (validImage) {
             imageSrc = validImage.src; // 첫 번째 유효한 이미지
@@ -127,32 +123,32 @@ const notice = (contents) => {
         <li class="list-item" data-id="${item.id}">
           <div class="text">
             <div class="date">${formattedDate}</div> 
-            <div class="title">${item.title.length > 14 ? item.title.substring(0, 14) + "..." : item.title}</div>
+            <div class="title">${item.title.length > 14 ? item.title.substring(0, 14) + '...' : item.title}</div>
             <div class="contents">
-              ${item.contents && item.contents.length > 50 ? item.contents.substring(0, 50) + "..." : item.contents || ""}
+              ${item.contents && item.contents.length > 50 ? item.contents.substring(0, 50) + '...' : item.contents || ''}
             </div>
           </div>
           <div class="img"><img src="${imageSrc}" alt="notice image"/></div>
         </li>
       `;
       })
-      .join("");
+      .join('');
 
     // 갤러리 모드
-    if (galleryList && galleryList.classList.contains("gallery-list")) {
+    if (galleryList && galleryList.classList.contains('gallery-list')) {
       galleryList.innerHTML = noticesHTML;
     }
 
     // 리스트 모드
-    if (list && list.classList.contains("list")) {
+    if (list && list.classList.contains('list')) {
       list.innerHTML = noticesHTML;
     }
   }
 
   function addClickEventToItems() {
-    const listItems = document.querySelectorAll(".list-item");
+    const listItems = document.querySelectorAll('.list-item');
     listItems.forEach((item) => {
-      item.addEventListener("click", () => {
+      item.addEventListener('click', () => {
         const noticeId = item.dataset.id; // data-id 가져오기
         window.location.href = `/notice/detail/${noticeId}`; // URL 파라미터로 전달
       });
@@ -160,13 +156,13 @@ const notice = (contents) => {
   }
 
   // 전체선택 버튼 클릭 시 아이콘 변경 로직
-  const allCheckButton = document.querySelector(".allCheckButton");
-  const checkBox = document.querySelector(".checkBox");
+  const allCheckButton = document.querySelector('.allCheckButton');
+  const checkBox = document.querySelector('.checkBox');
   if (allCheckButton && checkBox) {
-    allCheckButton.addEventListener("click", function () {
+    allCheckButton.addEventListener('click', function () {
       const currentSrc = checkBox.src;
 
-      if (currentSrc.includes("34bd4a1e-73f1-4ef8-896c-1aad0c1fed99")) {
+      if (currentSrc.includes('34bd4a1e-73f1-4ef8-896c-1aad0c1fed99')) {
         checkBox.src = CHECKED_ICON_URL; // 새로운 아이콘 경로로 변경
       } else {
         checkBox.src = UNCHECKED_ICON_URL; // 원래 아이콘으로 돌아가기
@@ -175,23 +171,23 @@ const notice = (contents) => {
   }
 
   // changeButton 클릭 시 목록 보기 방식  변경
-  const changeButton = document.querySelector(".changeButton");
-  const galleryList = document.querySelector(".gallery-list");
+  const changeButton = document.querySelector('.changeButton');
+  const galleryList = document.querySelector('.gallery-list');
 
   if (changeButton && galleryList) {
-    changeButton.addEventListener("click", function () {
-      if (galleryList.classList.contains("gallery-list")) {
-        galleryList.classList.remove("gallery-list");
-        galleryList.classList.add("list");
+    changeButton.addEventListener('click', function () {
+      if (galleryList.classList.contains('gallery-list')) {
+        galleryList.classList.remove('gallery-list');
+        galleryList.classList.add('list');
         changeButton.src = VIEW_ICON_URL; // 새로운 아이콘 경로로 변경
       } else {
-        galleryList.classList.remove("list");
-        galleryList.classList.add("gallery-list");
+        galleryList.classList.remove('list');
+        galleryList.classList.add('gallery-list');
         changeButton.src = LIST_ICON_URL; // 원래 아이콘으로 돌아가기
       }
     });
   } else {
-    console.warn("changeButton or galleryList not found.");
+    console.warn('changeButton or galleryList not found.');
   }
 };
 
