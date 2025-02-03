1b6762136aea99c068f3962992acbe48d231e62a
ijisun <ijisun9292@gmail.com>
2025-02-03 16:04:59 +0900

    refactor: 휴가관리 json 파일 load 실행 위치 변경

--- src/main.js
+++ src/main.js
@@ -54,19 +54,18 @@ const initializeLocalStorage = function () {
           console.error("Error loading JSON:", error);
         });
 
-      // 휴가관리 json 파일 load
-      fetch('/src/data/leaves.json')
-        .then((response) => response.json())
-        .then((data) => {
-          if (localStorage.getItem("leaves")) {
-            return;
-          }
-          localStorage.setItem("leaves", JSON.stringify(data));
-        })
-        .catch((error) => {
-          console.error("Error loading JSON:", error);
-        });
-
+    });
+  // 휴가관리 json 파일 load
+  fetch('/src/data/leaves.json')
+    .then((response) => response.json())
+    .then((data) => {
+      if (localStorage.getItem("leaves")) {
+        return;
+      }
+      localStorage.setItem("leaves", JSON.stringify(data));
+    })
+    .catch((error) => {
+      console.error("Error loading JSON:", error);
     });
 };
 
